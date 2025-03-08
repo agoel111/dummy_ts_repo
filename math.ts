2c2a822cf0349c139c5f1d302ac5b327276a398f
agoel111 <153257819+agoel111@users.noreply.github.com>
2025-03-08 16:34:21 +0530

    Create math.ts

---
+++ math.ts
@@ -0,0 +1,11 @@
+// math.ts - Exports functions
+export function add(a: number, b: number): number {
+    return a + b;
+}
+
+export function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+// Named export of a constant
+export const PI = 3.14159;
